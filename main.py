c30548cb1493572146b9436428d72e7501c04950
rzc0d3r <106631540+rzc0d3r@users.noreply.github.com>
2022-12-31 19:41:06 +0200

    31.12.2022 Update (Linking with new updates)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Version 1.0.1 (31.12.2022)
 import eset_license_generator_auto as eset_auto
 import time
 
@@ -5,11 +6,21 @@ SIZE = 1
 OUTPUT = 'eset_accounts.txt'
 SLEEP = 1
 
-
 for i in range(SIZE):
-    email, password = eset_auto.CreateALL()
-    f = open(OUTPUT, 'a')
-    f.write(f'{email} : {password}\n')
-    f.close()
+    data = eset_auto.CreateALL()
+    if data is not None:
+        email, password = data
+        try:
+            f = open(OUTPUT, 'a')
+            f.write(f'{email} : {password}\n')
+            f.close()
+        except:
+            print('[-] Error write account to file!!!')
+            break
+        print(f'[+] {email}:{password} - written successfully to output file!!!')
+    else:
+        print('[-] Error get account!!!')
+        break
     time.sleep(SLEEP)
+    
 input('Press Enter...')
