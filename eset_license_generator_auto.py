7b14d2f04e17f0c0003200bca4872e93c29073dd
rzc0d3r <106631540+rzc0d3r@users.noreply.github.com>
2023-01-05 01:55:32 +0200

    Code acceleration, smart IP ban check (05.01.2023)

--- eset_license_generator_auto.py
+++ eset_license_generator_auto.py
@@ -1,44 +1,47 @@
-# Version: 1.0.3.2 (04.01.2023)
-import eset_intercepter as ESET
-import selenium.webdriver
-import random
-import string
-import sys
+# Version: 1.0.4 (05.01.2023)
+import eset_intercepter
 import time
 
+from selenium.webdriver import Chrome
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.chrome.service import Service
+
+from string import ascii_letters
+from random import choice
+
 def CreateEmailAndPassword():
-    email_obj = ESET.Email()
+    email_obj = eset_intercepter.Email()
     email_obj.register()
     email = email_obj.get_full_login()
-    password = [random.choice(string.ascii_lowercase), random.choice(string.ascii_uppercase), random.choice('0123456789')]
-    password += [random.choice(string.ascii_letters+'0123456789') for _ in range(17)]
-    password = ''.join(password)
+    password = ''.join(['Aa0$']+[choice(ascii_letters) for _ in range(6)])
     return email_obj, email, password
     
 def CreateAccount(email, password):
-    driver = selenium.webdriver.Chrome('chromedriver.exe')
+    driver_options = ChromeOptions()
+    driver_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+    driver = Chrome(service=Service('chromedriver.exe'), options=driver_options)
     driver.minimize_window()
+
     driver.get(f'https://login.eset.com/Register')
-    js = [
-        f"document.getElementById('Email').value = '{email}'",
-        "document.forms[0].submit()"
-    ]
-    driver.execute_script('\n'.join(js))
-    js = [
-       f"document.getElementById('Password').value = '{password}'",
-        "document.forms[0].submit()"
-    ]
-    driver.execute_script('\n'.join(js))
-    time.sleep(2)
-    title = driver.execute_script('return document.title')
-    if title == 'Service not available':
-        print('[-] ESET temporarily blocked your IP, try again later!!!')
-        driver.quit()
-        return None
-    return driver
+    submit, getbyid = 'document.forms[0].submit()', 'document.getElementById'
+    driver.execute_script(f"{getbyid}('Email').value='{email}'\n{submit}")
+    driver.execute_script(f"{getbyid}('Password').value='{password}'\n{submit}")
+
+    while True:
+        time.sleep(0.1)
+        title = driver.execute_script('return document.title')
+        if title == 'Service not available':
+            print('[-] ESET temporarily blocked your IP, try again later!!!')
+            driver.quit()
+            break
+        url = driver.execute_script('return document.URL')
+        if url == 'https://home.eset.com/':
+            return driver
+    return None
 
 def GetToken(email_obj):
-    return ESET.ESETIntercepter(email_obj)
+    return eset_intercepter.eset_intercepter(email_obj, 0.5)
 
 def ConfirmAccount(driver, token):
     driver.get(f'https://login.eset.com/link/confirmregistration?token={token}')
@@ -57,10 +60,10 @@ def CreateALL(print_eset_token=True, print_account_data=False):
             print(f'\nEmail: {email}\nPassword: {password}')
         ConfirmAccount(driver, token)
         return email, password
-    except ESET.EmailConnectError:
+    except eset_intercepter.EmailConnectError:
         print('[-] Error connect to server!!!')
     except Exception as E:
-        print(E)
+        print(f'[-] {E}')
     return None
 
 if __name__ == '__main__':
